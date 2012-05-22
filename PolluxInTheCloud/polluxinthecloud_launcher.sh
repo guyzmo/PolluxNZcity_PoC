d38903039054d8d95b4d68566bda154426f856e6
Guyzmo <guyzmo+github@m0g.net>
2012-05-22 17:45:01 +0200

    minor fixes

--- PolluxInTheCloud/polluxinthecloud_launcher.sh
+++ PolluxInTheCloud/polluxinthecloud_launcher.sh
@@ -2,8 +2,9 @@
 
 cd `dirname $0`
 if [ -x /usr/bin/x-www-browser ]; then
-    /usr/bin/env x-www-browser http://127.0.0.1:8000/ &
-    /usr/bin/env python ./polluxinthecloud.py -p 8000 -vvv
+    /usr/bin/env x-www-browser http://127.0.0.1/ &
+    echo "Please input your user password to use 'sudo': "
+    sudo /usr/bin/env python ./polluxinthecloud.py -p 80 -vvv
 else
     echo "couldn't find a browser. Press a key to close this program"
     read
